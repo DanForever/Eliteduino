2ed5ab60db6391900732915e5586c7b32076b27b
Dan Neve <6909667+DanForever@users.noreply.github.com>
2021-04-05 00:12:51 +0200

    Fixed devices.h defines in repository
    The devices shouldn't be explicitly defined in the codebase itself (it's a failing of the arduino ide that they need to be enabled via code rather than compiler settings)

--- ControlPanel/src/Devices.h
+++ ControlPanel/src/Devices.h
@@ -9,7 +9,7 @@ template<class T, int N>
 constexpr int ArraySize( T( & )[ N ] ) { return N; }
 
 // Temporary until proper device configurations are figured out and defined
-#define ELITEDUINO_DEVICE_TEENSY_BUTTONSANDSCREEN
+//#define ELITEDUINO_DEVICE_TEENSY_BUTTONSANDSCREEN
 //#define ELITEDUINO_DEVICE_PROMICRO_16BUTTONSONLY
 
 //------------------------------------------------------------------------------------------------
@@ -37,9 +37,9 @@ const uint8_t LED_CONTROLLER_ROW_COUNT = ArraySize( BUTTON_ROWS );
 
 #ifdef ELITEDUINO_DEVICE_TEENSY_BUTTONSANDSCREEN
 
-// todo: Actual values (for now they're copied from the pro micro)
-const uint8_t BUTTON_COLUMNS[] = {1};
-const uint8_t BUTTON_ROWS[] = {2};
+// todo: Actual values
+const uint8_t BUTTON_COLUMNS[] = {14};
+const uint8_t BUTTON_ROWS[] = {0};
 const uint8_t BUTTON_COLUMN_COUNT = ArraySize( BUTTON_COLUMNS );
 const uint8_t BUTTON_ROW_COUNT = ArraySize( BUTTON_ROWS );
 const uint8_t DEBOUNCE_INTERVAL = 25;
